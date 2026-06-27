bd7c472348db49c7c904dd5a299db9426f59e393
akshat <akshatsinghdelhi@gmail.com>
2026-06-27 20:35:28 +0530

    Polish CLI output and improve error handling

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,9 +1,11 @@
 import { BunRuntime, BunServices } from "@effect/platform-bun";
+import * as Console from "effect/Console";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
 import * as Command from "effect/unstable/cli/Command";
 
 import { cli } from "./command";
+import { formatCliCause, isHelpCause } from "./lib/cli-output";
 import { ApiClient } from "./services/api-client";
 import { AuthClient } from "./services/auth.client";
 import { UserConfig } from "./services/user-config";
@@ -27,5 +29,16 @@ const AppLive = Layer.mergeAll(
 cli.pipe(
   Command.run({ version: packageJson.version }),
   Effect.provide(AppLive),
+  Effect.catchCause((cause) =>
+    Effect.gen(function* handleCliCause() {
+      if (!isHelpCause(cause)) {
+        yield* Console.error(formatCliCause(cause));
+      }
+
+      yield* Effect.sync(() => {
+        process.exitCode = 1;
+      });
+    })
+  ),
   BunRuntime.runMain
 );
