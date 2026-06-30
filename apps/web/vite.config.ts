05b782b4bb0886d6a06a57d2229132def35b6b9b
akshat <akshatsinghdelhi@gmail.com>
2026-06-30 09:04:57 +0530

    feat: use nitro for production web

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,10 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    nitro({ preset: "bun" }),
+    viteReact(),
+  ],
   resolve: { tsconfigPaths: true },
 });
 
